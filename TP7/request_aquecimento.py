2a547cbfbaafb50bafe2493f4230b1ec0209802a
ZeCosta <josepedroc.costa@gmail.com>
2022-04-11 15:01:13 +0100

    Adiçao de paragrafos na view index. Resoluçao do exercicio de aquecimento

--- TP7/request_aquecimento.py
+++ TP7/request_aquecimento.py
@@ -1,9 +1,49 @@
 import requests
 import json
 
-#1
 apikey = open("apiKey","r").read()
+
+
+#1
+resposta1=[]
 resp = requests.get( 
-    "http://clav-api.di.uminho.pt/v2/classes?nivel=3&apikey="+apikey
+    "http://clav-api.di.uminho.pt/v2/classes/c150?&apikey="+apikey
 ) 
-print(json.dumps(resp.json(), indent=4, sort_keys=True))
+for filho2 in resp.json()["filhos"]:
+    resp2 = requests.get( 
+        "http://clav-api.di.uminho.pt/v2/classes/c"+filho2["codigo"]+"?&apikey="+apikey
+    )
+    resposta1=resposta1 + resp2.json()["filhos"]
+print(len(resposta1))
+
+for elem in resposta1:
+    print(elem)
+
+print("\n\n------------------------\n------------------------\n\n")
+
+resposta2 = requests.get( 
+    "http://clav-api.di.uminho.pt/v2/classes?nivel=4&apikey="+apikey
+).json()
+print(len(resposta2))
+for elem in resposta2:
+    print(elem)
+
+print("\n\n------------------------\n------------------------\n\n")
+
+resposta3 = requests.get( 
+    "http://clav-api.di.uminho.pt/v2/classes/c750.30?apikey="+apikey
+).json()
+print(len(resposta3["filhos"]))
+for elem in resposta3["filhos"]:
+    print(elem)
+
+print("\n\n------------------------\n------------------------\n\n")
+
+resposta4 = requests.get( 
+    "http://clav-api.di.uminho.pt/v2/classes/c750.30.001?apikey="+apikey
+).json()
+print(len(resposta4["processosRelacionados"]))
+for elem in resposta4["processosRelacionados"]:
+    print(elem)
+
+    
